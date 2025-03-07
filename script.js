015f687d5bc84609dfc690e9b1e67ef91d9d4a52
dianmei1602 <dianmei1602@gmail.com>
2025-03-07 06:08:17 +0000

    Animation Changes

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ const menuIcon = document.querySelector('.menu box-icon');
             navElement.classList.remove('open'); // Hapus kelas 'open'
         }
     });
+// WINDOW SCROLL
+function setActiveNav() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav a');
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+            const id = section.getAttribute('id');
+
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${id}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+}
+
+window.addEventListener('scroll', setActiveNav);
+setActiveNav(); // Panggil saat halaman dimuat untuk inisialisasi
